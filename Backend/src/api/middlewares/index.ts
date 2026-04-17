c2ba845228d3c1a93e7122c6c6dedfadeb55dcd3
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-04-17 15:47:48 +0530

    index.ts added to middlewares

---
+++ Backend/src/api/middlewares/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Central export point for all API middlewares.
+ * Simplifies imports in the main server and route files.
+ */
+export * from './authMiddleware';
+export * from './socketAuthMiddleware';
+export { generalLimiter, uploadLimiter } from './rateLimiter';
+export { errorHandler } from './errorHandler';
