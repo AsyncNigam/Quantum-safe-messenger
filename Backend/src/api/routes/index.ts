4e91a838022bfeaaa305196a875c021eed75ae4e
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-04-17 15:46:18 +0530

    index.ts added to routes

--- Backend/src/api/routes/index.ts
+++ Backend/src/api/routes/index.ts
@@ -1,4 +1,8 @@
 import keyRoutes    from './keyRoutes';
 import healthRoutes  from './healthRoutes';
 
+/**
+ * Central export point for all API routes.
+ * Simplifies imports in the main server file.
+ */
 export { keyRoutes, healthRoutes };
